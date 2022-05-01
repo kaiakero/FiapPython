4b971f1abe2be078fdaafd90e815d193463d773c
Vitor Pinto <98500066+kaiakero@users.noreply.github.com>
2022-05-01 19:20:33 -0300

    Add files via upload

---
+++ ModuloPrincipal.py
@@ -0,0 +1,16 @@
+from DefinindoFuncoes import *
+lista=[]
+print('preenchendo')
+preencherInventario(lista)
+print('exibindo')
+exibirInventario(lista)
+print('pesquisando')
+localizarPorNome(lista)
+print('depreciacao')
+DepreciarPorNome(lista)
+print(Excluindo)
+print(excluirPorSerial(lista))
+print('Resumindo valores')
+resumirValores(lista)
+
+
